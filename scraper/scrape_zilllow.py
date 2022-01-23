9f98cd52d932d2c05c967a55e23344e6b8b9a2bb
jbofill10 <juan.bofill10@gmail.com>
2022-01-23 04:40:54 -0500

    add basic url generation

--- scraper/scrape_zilllow.py
+++ scraper/scrape_zilllow.py
@@ -1,8 +1,22 @@
 from bs4 import BeautifulSoup
+from zillow_payload import ZillowPayload
 
 
 def main():
-	pass
+	state_filters = dict(
+		filterState=dict(
+			price=dict(
+				min=100000,
+				max=1000000
+			),
+			beds=dict(min=3),
+			baths=dict(min=2)
+		)
+	)
+
+	zillow_query = ZillowPayload('Austin, TX', state_filters)
+
+	print(zillow_query.get_url())
 
 if __name__ == '__main__':
 	main()
--- scraper/zillow_payload.py
+++ scraper/zillow_payload.py
@@ -1,3 +1,6 @@
+import urllib
+import json
+
 class ZillowPayload:
 	"""
 	Used to store the search parameters for a house query
@@ -7,23 +10,40 @@ class ZillowPayload:
 	# setter functions (if they are used) will handle the
 	# actual dictionary	structure
 
-	state_filters = dict(
-		price=None,
-		beds=None,
-		baths=None,
-		basement=None,
-		sqft=None,
-		lot_size=None,
-		parking_spots=None,
-		single_family=False,
-		apartment=False,
-		town_house=False,
-		multi_family=False,
-		manufactured=False,
-		lots_or_land=False,
+	ZILLOW_BASE_URL = 'https://www.zillow.com/homes'
+
+	filters = dict(
+		usersSearchTerm=None,
+		filterState=dict(
+			price=None,
+			beds=None,
+			baths=None,
+			# garage
+			gar=dict(value=None)
+			# basement finished
+			basf=dict(value=None),
+			# basement unfinished
+			basu=dict(value=None)
+			sqft=None,
+			lot_size=None,
+			parking_spots=None,
+			single_family=False,
+			apartment=False,
+			town_house=False,
+			multi_family=False,
+			manufactured=False,
+			lots_or_land=False,
+		)
 	)
 
-	def __init__(self, search_term: str, state_filter: dict, sort_filter: str) -> None:
+	def __init__(self, search_term: str, state_filters: dict, sort_filter: str = '') -> None:
+		self.search_term = search_term
+		self.state_filters = state_filters
+
+		# Duplicate storage, but makes sense when I dump to JSON, so fuck it
+		self.state_filters['usersSearchTerm'] = self.search_term
+
+		self.sort_filter = sort_filter
 		pass
 
 	def set_search_term(self, search_term: str) -> None:
@@ -32,25 +52,17 @@ class ZillowPayload:
 	def set_state_filters(self, state_filter: dict) -> None:
 		pass
 
-	def _set_numeric_state_filters(self, filter_type: str, min, max):
+	def get_url(self) -> str:
 		"""
-		Sets numerical filter values like
-		price, beds, baths, etc.
-			filter_type: The filter to edit
-			min: The minimum amount
-			max: The maximum amount
+		Prepares url using data held in the payload object
+		@returns:
+			url string to get html from the query
 		"""
+		url_without_encoding = f'{self.ZILLOW_BASE_URL}/{self.search_term}_rb/?searchQueryState='
+		url_to_encode = urllib.parse.quote(
+			json.dumps(
+				self.state_filters,
+				separators=(',', ':'))
+		)
 
-		self.state_filters[filter_type]['min'] = min
-		self.state_filters[filter_type]['max'] = max
-
-	def _set_bool_state_filter(self, filter_type: str, val: bool) -> None:
-		"""
-		Sets filter condition for values such as
-		single_family, apartment, etc.
-			filter_type: The filter to edit
-			val: Boolean value of whether to use it or not
-		"""
-
-		self.state_filters[filter_type] = val
-
+		return url_without_encoding + url_to_encode
