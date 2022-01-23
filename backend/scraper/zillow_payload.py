3987fda0db32b6990ca0143cc326169296a1bc05
jbofill10 <juan.bofill10@gmail.com>
2022-01-23 15:29:01 -0500

    get query working move scraper files into backend folder

--- backend/scraper/zillow_payload.py
+++ backend/scraper/zillow_payload.py
@@ -1,5 +1,6 @@
 import urllib
 import json
+from warnings import filters
 
 class ZillowPayload:
 	"""
@@ -14,36 +15,36 @@ class ZillowPayload:
 	filters = dict(
 		filterState=dict()
 	)
-	'''
-	Zillow filter structure for reference
-
-		filterState=dict(
-			price=None,
-			beds=dict(min=None),
-			baths=dict(min=None),
-			# garage
-			gar=dict(value=True),
-			# basement finished
-			basf=dict(value=True),
-			# basement unfinished
-			basu=dict(value=True),
-			sqft=dict(min=2000, max=5000),
-			lot=dict(min=2000, max=5000),
-			# parking spots, no clue why zillow calls it "parks"
-			parks=dict(value=1),
-			# single family home
-			sf=dict(value=True),
-			# apartment
-			apa=dict(value=False),
-			# town home
-			tow=dict(value=False),
-			# multi-family home
-			mf=dict(value=False),
-			# manufactured home
-			manu=dict(value=False),
-			lots or land
-			land=dict(value=False)
-	'''
+	# filters = dict(
+	# 	usersSearchTerm=None,
+	# 	filterState=dict(
+	# 		price=None,
+	# 		beds=dict(min=None),
+	# 		baths=dict(min=None),
+	# 		# garage
+	# 		gar=dict(value=True),
+	# 		# basement finished
+	# 		basf=dict(value=True),
+	# 		# basement unfinished
+	# 		basu=dict(value=True),
+	# 		sqft=dict(min=2000, max=5000),
+	# 		lot=dict(min=2000, max=5000),
+	# 		# parking spots, no clue why zillow calls it "parks"
+	# 		parks=dict(value=1),
+	# 		# single family home
+	# 		sf=dict(value=True),
+	# 		# apartment
+	# 		apa=dict(value=False),
+	# 		# town home
+	# 		tow=dict(value=False),
+	# 		# multi-family home
+	# 		mf=dict(value=False),
+	# 		# manufactured home
+	# 		manu=dict(value=False),
+	# 		# lots or land
+	# 		land=dict(value=False)
+	# 	)
+	# )
 
 	def __init__(self, search_term: str, state_filters: dict, sort_filter: str = '') -> None:
 
@@ -57,7 +58,7 @@ class ZillowPayload:
 		self.sort_filter = sort_filter
 		pass
 
-	def update_filter(self, search_term: str, filters: dict) -> None:
+	def update_filter(self, search_term: str, filter: dict) -> None:
 		"""
 		Updates filters after zillow singleton exists if necessary to update
 		"""
